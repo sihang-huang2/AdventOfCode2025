7b6a4a2f3c4ebe7326b0ed0f0196f866b8cc4241
sihang-huang2 <sihangh@nycstudents.net>
2025-12-04 19:03:51 +0000

    b

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+//hi
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Day2 {
--- Day3/Day3.java
+++ Day3/Day3.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+//hi
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Day3 {
